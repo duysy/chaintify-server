34628a88deda38fa2b103e2890d7b9651a8b149c
Duy Sy Nguyen <duyduysysy@gmail.com>
2023-01-12 22:14:26 +0700

    update

--- app/music/config.py
+++ app/music/config.py
@@ -8,14 +8,14 @@ class Config:
     PORT = '5432'
 
     # HOST = 'localhost'
-    # BASE_GETAWAY = "https://6307-2a09-bac1-7ac0-30-00-19b-18e.ap.ngrok.io/ipfs"
+    # BASE_GETAWAY = "https://8285-2a09-bac1-7ac0-30-00-19b-18e.ap.ngrok.io/ipfs"
     # URL_API_IPFS = "/ip4/127.0.0.1/tcp/5001"
 
-    BASE_GETAWAY = "https://6307-2a09-bac1-7ac0-30-00-19b-18e.ap.ngrok.io/ipfs"
-    URL_API_IPFS = "/dns/ipfs/tcp/5001/http"
-
-    # BASE_GETAWAY = "http://149.28.157.139:8080/ipfs"
+    # BASE_GETAWAY = "https://8285-2a09-bac1-7ac0-30-00-19b-18e.ap.ngrok.io/ipfs"
     # URL_API_IPFS = "/dns/ipfs/tcp/5001/http"
+
+    BASE_GETAWAY = "http://103.39.125.95:8080/ipfs"
+    URL_API_IPFS = "/dns/ipfs/tcp/5001/http"
 
     ALCHEMY_KEY = "Gnp3tbW0Wp0tJpdhVz1ZRtB9bqgYG03O"
     CHAINTIFY_CONTRACT_ADDRESS = "0xAC9676B8dacAfe02986B35d2D045724C7D1B9F93"
